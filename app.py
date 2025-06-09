0d54c926296b4cf7a654401dbaee7c023ccacb59
helen-tesfay <moonchaerries@gmail.com>
2025-06-09 18:05:46 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ custom_css = """
 <style>
     /* Main page background */
     [data-testid="stAppViewContainer"] {
-        background-image: url("https://preview.colorkit.co/color/5A0000.png?type=article-preview-logo&size=social&colorname=Spikey%20Red");
+        background-image: url("https://images.unsplash.com/flagged/photo-1593005510509-d05b264f1c9c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
         background-size: cover;
         background-repeat: no-repeat;
         background-position: center;
